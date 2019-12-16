eca00654f81dc6efc2497bf45005b60a28eca699
V-gy <vgy@hotmail.fr>
2019-12-16 11:36:12 +0100

    git tag step-1

---
+++ capitalizeFirst.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// function capitalizeFirst(input) {
+//     let output = '';
+//     for (let i = 0 ; i < input.length ; i++) {
+//       if (i === 0) {
+//         output += input[0].toUpperCase();
+//       } else {
+//         output += input[i];
+//       }
+//     }
+//     return output;
+//   }
+
+  function capitalizeFirst(input) {
+    return input.length > 0
+      ? input[0].toUpperCase() + input.slice(1)
+      : '';
+  }
+
+
+  module.exports = capitalizeFirst;
