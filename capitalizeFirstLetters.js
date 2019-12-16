b72a74848a91098ced0ccb2eda55745936c40bb2
V-gy <vgy@hotmail.fr>
2019-12-16 11:01:27 +0100

    tests passed

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,12 @@
 'use strict';
 
+
+function capitalizeFirstLetters(input) { 
+    return input.split(" ").map(function(word){
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(" ");
+  }
+
 // capitalizeFirst.js
 const assert = require('assert');
 
@@ -12,7 +19,7 @@ assert.strictEqual(capitalizeFirstLetters.length, 1);
 // Check that capitalizeFirstLetters transforms javaScript correctly
 assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
 // Check that capitalizeFirstLetters transforms i am learning TDD correctly
-assert.strictEqual(capitalzeFirstLetters('i am learning TDD '), 'I Am Learning TDD');
+assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
 // Check that it works for a 1-character string
 assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
 // Check that it works for an empty string
